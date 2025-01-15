32666b3baf0a768445f73e47d998383003db3941
kuznetsovadaria-it16 <kuzdashad@gmail.com>
2025-01-15 22:12:13 +0500

    Add files via upload

---
+++ заг.h
@@ -0,0 +1,10 @@
+#ifndef «¿√_H
+#define «¿√_H
+#include <string>
+#include <iostream>
+using namespace std;
+long long NOD(long long a, long long b);
+string NOK(const string& a, const string& b);
+bool isvalidinput(const string& input);
+#endif
+
